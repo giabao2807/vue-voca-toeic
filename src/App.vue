5b75d9fa7f46d84adcffb93f9069ee571f0954f6
Dinh Gia Bao <dinhgiabao2807@gmail.com>
2022-07-06 16:34:59 +0700

    Blog Post components for home view

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
     <div class="app">
       <Navigation />
       <router-view />
-      <Footer/>
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation.vue";
-import Footer from "./components/Footer.vue"
+import Footer from "./components/Footer.vue";
 export default {
   name: "app",
   components: {
     Navigation,
-    Footer
+    Footer,
   },
   data() {
     return {};
@@ -57,5 +57,19 @@ export default {
 
 .link-light {
   color: #fff;
+}
+
+.arrow {
+  margin-left: 8px;
+  width: 12px;
+  path {
+    fill: #000;
+  }
+}
+
+.arrow-light {
+  path {
+    fill: #fff;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome to Giabao's blog",
+        blogPost:
+          "Weekly blog posts from the best authors in the world.\n" +
+          "Join the community and share your thoughts with the world about anime.",
+        welcomeScreen: true,
+        photo: "blog1",
+      },
+      sampleBlogPost: [
+        {
+          title: "Sample Blog Post",
+          blogHTMl: "This is a filler blog post. It is not meant to be read.",
+          blogCoverPhoto: "blog2",
+        },
+        {
+          title: "Sample Blog Post",
+          blogHTMl: "This is a filler blog post. It is not meant to be read.",
+          blogCoverPhoto: "blog3",
+        },
+      ],
+    };
+  },
 };
 </script>
