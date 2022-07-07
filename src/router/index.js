7df8ff79fdb5d0793f3170eb11f86ae428b6eba5
Dinh Gia Bao <dinhgiabao2807@gmail.com>
2022-07-07 15:24:28 +0700

    title for blog

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,17 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home"
+    }
   },
   {
     path: "/blogs",
     name: "Blogs",
     component: Blogs,
+    meta: {
+      title: "Blogs"
+    }
   },
 ];
 
@@ -22,5 +28,10 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to,from, next) => {
+    document.title = `${to.meta.title} | Giabao`;
+    next();
+})
 
 export default router;
